792b2984f0044d172266e7efab5eedc118f563af
DavidBergevoet <epicman008@gmail.com>
2021-12-20 20:17:05 +0100

    Day 14 part 2 FINALLY DONE

--- 2021/14/part2.cpp
+++ 2021/14/part2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <map>
+#include <unordered_map>
 #include "FileHandler.hpp"
 
 int main(int argc, char const *argv[])
@@ -9,76 +10,102 @@ int main(int argc, char const *argv[])
     std::string beginSequence;
     file.GetLine(beginSequence);
     std::cout << "BeginSequence: " << beginSequence << std::endl;
+    char last = beginSequence[beginSequence.size()-1];
 
-    std::vector<std::pair<std::string, char>> insertionRules;
     std::map<std::string,char> insertionMap;
+    std::unordered_map<std::string, uint64_t> pairMap;
+
+    for(size_t i = 0; i < beginSequence.size()-1; ++i)
+    {
+        std::string pair;
+        pair += beginSequence[i];
+        pair += beginSequence[i+1];
+        if(pairMap.find(pair) != pairMap.end())
+        {
+            pairMap[pair] += 1;
+        }
+        else
+        {
+            pairMap[pair] = 1;
+        }
+    }
+
+    // std::cout<<"PairMap:"<<std::endl;
+    // for(std::unordered_map<std::string, uint64_t>::iterator it = pairMap.begin(); it != pairMap.end(); ++it)
+    // {
+    //     std::cout<<"\tKey: "<<it->first<<"\tVal: "<<it->second<<std::endl;
+    // }
+
     std::vector<std::string> splitLine;
     while(file.GetLineSplit(splitLine, " -> "))
     {
         if(splitLine.size() != 0)
-        {
-            insertionRules.push_back(std::pair<std::string, char>(splitLine[0],splitLine[1][0]));
             insertionMap[splitLine[0]] = splitLine[1][0];
-        }
     }
 
-    std::string previousSequence = beginSequence;
-    std::string newSequence;
-    for(size_t i = 0; i < 40; ++i)
+    // std::cout<<"InsertionMap:"<<std::endl;
+    // for(std::map<std::string, char>::iterator it = insertionMap.begin(); it != insertionMap.end(); ++it)
+    // {
+    //     std::cout<<"\tKey: "<<it->first<<"\tVal: "<<it->second<<std::endl;
+    // }
+
+    std::unordered_map<std::string, uint64_t> tempPairMap, newPairMap = pairMap;
+    for(size_t step = 0; step < 40; ++step)
     {
-        std::cout<<"Step: "<<i<<std::endl;
-        newSequence = "";
-        newSequence += previousSequence[0];
-        for(size_t i = 0; i < previousSequence.size() - 1; ++i)
+        // std::cout<<"Step:"<<step<<std::endl;
+        tempPairMap = newPairMap;
+        newPairMap.clear();
+        std::string firstPair, secondPair;
+        for(auto it = tempPairMap.begin(); it != tempPairMap.end(); ++it)
         {
-            std::string checkString;
-            (checkString += previousSequence[i]) += previousSequence[i + 1];
-            // std::cout << "CheckS: "<<checkString<<std::endl; 
+            firstPair = secondPair = "";
+            char middleChar = insertionMap[it->first];
+            firstPair = it->first[0];
+            firstPair += middleChar;
+            secondPair += middleChar;
+            secondPair += it->first[1];
+            // std::cout<<"F: "<<firstPair<<" S: "<<secondPair<<std::endl;
 
-            newSequence += insertionMap[checkString]; 
+            if(newPairMap.find(firstPair) != newPairMap.end())
+                newPairMap[firstPair] += it->second;
+            else
+                newPairMap[firstPair] = it->second;
 
-            // for(const auto& rRule : insertionRules)
-            // {
-            //     if(rRule.first == checkString)
-            //     {
-            //         newSequence += rRule.second;
-            //         break;
-            //     }
-            // }
-            newSequence += checkString[1];
+            if(newPairMap.find(secondPair) != newPairMap.end())
+                newPairMap[secondPair] +=  it->second;
+            else
+                newPairMap[secondPair] = it->second;
+
         }
-        previousSequence = newSequence;
     }
-    // std::cout<<"Final sequence: "<<newSequence<<std::endl;
+    
+    // std::cout<<"NewPairMap:"<<std::endl;
+    // for(std::unordered_map<std::string, uint64_t>::iterator it = newPairMap.begin(); it != newPairMap.end(); ++it)
+    // {
+    //     std::cout<<"\tKey: "<<it->first<<"\tVal: "<<it->second<<std::endl;
+    // }
 
-    std::vector<char> uniqueCharacters;
-    for(char character : newSequence)
+    uint64_t mostCommon = 0, 
+        leastCommon = UINT64_MAX;
+
+    std::unordered_map<char, uint64_t> charCount;
+    for(std::unordered_map<std::string, uint64_t>::iterator it = newPairMap.begin(); it != newPairMap.end(); ++it)
     {
-        bool isIn = false;
-        for(char unique : uniqueCharacters)
-        {
-            if(character == unique)
-            {
-                isIn = true;
-                break;
-            }
-        }
-        if(!isIn)
-            uniqueCharacters.push_back(character);
+        if(charCount.find(it->first[0]) != charCount.end())
+            charCount[it->first[0]] += it->second;
+        else
+            charCount[it->first[0]] = it->second;
     }
-
-
-    uint64_t mostCommon = 0, leastCommon = UINT64_MAX;
-
-    for(char unique : uniqueCharacters)
+    charCount[last] += 1;
+    
+    for(std::unordered_map<char, uint64_t>::iterator it = charCount.begin(); it != charCount.end(); ++it)
     {
-        uint32_t count = std::count(newSequence.begin(), newSequence.end(), unique);
-        if(count > mostCommon)
-            mostCommon = count;
-        if(count < leastCommon)
-            leastCommon = count;
+        // std::cout<<it->first<<"\t"<<it->second<<std::endl;
+        if(it->second > mostCommon)
+            mostCommon = it->second;
+        if(it->second < leastCommon)
+            leastCommon = it->second;
     }
-
     std::cout<<"Most common: "<<mostCommon<<std::endl;
     std::cout<<"Least common: "<<leastCommon<<std::endl;
     std::cout<<mostCommon-leastCommon<<std::endl;
