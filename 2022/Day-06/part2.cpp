adb69f03055d9eac29689209f7f0085cfae437ee
David Bergevoet <epicman008@gmail.com>
2022-12-06 08:11:45 +0100

    Easy peasy star fruit squeezy

---
+++ 2022/Day-06/part2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+#include "FileHandler.hpp"
+
+int main(int argc, char const *argv[])
+{
+    std::string filePath = "input.txt";
+
+    uint64_t characterIndex = 0;
+
+    std::vector<char> lastCharacters(14);
+
+    FileHandler file(filePath);
+    if (file.IsOpen())
+    {
+        // Read the file
+        std::string line;
+        file.GetLine(line);
+
+        for (uint64_t i = 0; i < line.size(); ++i)
+        {
+            uint index = i % lastCharacters.size();
+            lastCharacters[index] = line[i];
+
+            bool different = true;
+            for (uint c = 0; c < lastCharacters.size(); ++c)
+            {
+                if (lastCharacters[c] == 0)
+                {
+                    different = false;
+                    break;
+                }
+                for (uint oc = 0; oc < lastCharacters.size(); ++oc)
+                {
+                    if (oc != c)
+                    {
+                        if (lastCharacters[oc] == lastCharacters[c])
+                        {
+                            different = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            if(different)
+            {
+                std::cout<<"Diff on character: "<<i+1<<std::endl;
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
