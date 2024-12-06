5e48a13906b15ac78cc0cf4ce6d60de64e94c36c
David Bergevoet <david@rometron.nl>
2024-12-06 17:08:24 +0100

    Janke met de pet op

--- 2024/Day-06/part2.cpp
+++ 2024/Day-06/part2.cpp
@@ -128,30 +128,88 @@ int main(int argc, char const* argv[])
     std::vector<std::pair<Point_t, Direction_e>> obsHit;
     Point_t guardCopy = originalGuard;
 
-    // Turn guard
-    switch (dir)
-    {
-    case Direction_e::UP:
-      if (map[guardCopy.first - 1][guardCopy.second])
-        dir = Direction_e::RIGHT;
-      break;
-    case Direction_e::RIGHT:
-      if (map[guardCopy.first][guardCopy.second + 1])
-        dir = Direction_e::DOWN;
-      break;
-    case Direction_e::DOWN:
-      if (map[guardCopy.first + 1][guardCopy.second])
-        dir = Direction_e::LEFT;
-      break;
-    case Direction_e::LEFT:
-      if (map[guardCopy.first][guardCopy.second - 1])
-        dir = Direction_e::UP;
-      break;
-    }
-
-    // Move guard
     while (true)
     {
+      // Turn guard
+      Point_t nextPos(0, 0);
+      bool first = true;
+      bool looped = false;
+      Point_t obHit = MakePoint(-1, -1);
+      Direction_e oldDir = dir;
+      while (map[nextPos.first][nextPos.second] || first)
+      {
+        first = false;
+        switch (dir)
+        {
+        case Direction_e::UP:
+          if (map[guardCopy.first - 1][guardCopy.second])
+          {
+            obHit = MakePoint(guardCopy.first - 1, guardCopy.second);
+            dir = Direction_e::RIGHT;
+          }
+          else
+          {
+            nextPos = MakePoint(guardCopy.first - 1, guardCopy.second);
+          }
+          break;
+        case Direction_e::RIGHT:
+          if (map[guardCopy.first][guardCopy.second + 1])
+          {
+            obHit = MakePoint(guardCopy.first, guardCopy.second + 1);
+            dir = Direction_e::DOWN;
+            nextPos = MakePoint(guardCopy.first + 1, guardCopy.second);
+          }
+          else
+          {
+            nextPos = MakePoint(guardCopy.first, guardCopy.second + 1);
+          }
+          break;
+        case Direction_e::DOWN:
+          if (map[guardCopy.first + 1][guardCopy.second])
+          {
+            obHit = MakePoint(guardCopy.first + 1, guardCopy.second);
+            dir = Direction_e::LEFT;
+            nextPos = MakePoint(guardCopy.first, guardCopy.second - 1);
+          }
+          else
+          {
+            nextPos = MakePoint(guardCopy.first + 1, guardCopy.second);
+          }
+          break;
+        case Direction_e::LEFT:
+          if (map[guardCopy.first][guardCopy.second - 1])
+          {
+            obHit = MakePoint(guardCopy.first, guardCopy.second - 1);
+            dir = Direction_e::UP;
+            nextPos = MakePoint(guardCopy.first - 1, guardCopy.second);
+          }
+          else
+          {
+            nextPos = MakePoint(guardCopy.first, guardCopy.second - 1);
+          }
+          break;
+        }
+      }
+      if (obHit.first != -1)
+      {
+        for (const auto& rObs : obsHit)
+        {
+          if (rObs.first == obHit && rObs.second == oldDir)
+          {
+            looped = true;
+            obstacles++;
+            break;
+          }
+        }
+        obsHit.push_back(std::make_pair(obHit, oldDir));
+      }
+      if (looped)
+      {
+        // std::cout << "Loopie" << std::endl;
+        break;
+      }
+
+      // Move guard
       switch (dir)
       {
       case Direction_e::UP:
@@ -167,8 +225,23 @@ int main(int argc, char const* argv[])
         guardCopy.second--;
         break;
       }
-    }
 
+      // Check if inside grid
+      bool isInside = true;
+      for (const Point_t& rPoint : map.GetOutsidePoints())
+      {
+        if (rPoint.first == guardCopy.first && rPoint.second == guardCopy.second)
+        {
+          isInside = false;
+          break;
+        }
+      }
+      if (!isInside)
+      {
+        // std::cout << "Outside grid!" << guardCopy.first << ' ' << guardCopy.second << std::endl;
+        break;
+      }
+    }
     map[rPoint.first][rPoint.second] = false;
   }
   std::cout << "obs: " << obstacles << std::endl;
