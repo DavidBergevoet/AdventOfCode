cd6632c8401d6c1a538df5f43664b9820437fe5e
David Bergevoet <david@rometron.nl>
2024-12-09 21:19:08 +0100

    Every day im shuffeling/struggling!

--- 2024/Day-09/part2.cpp
+++ 2024/Day-09/part2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include <vector>
 
 #include "FileHandler.hpp"
@@ -11,11 +12,12 @@ int main(int argc, char const* argv[])
   FileHandler file(filePath);
 
   std::vector<int> separatedLine;
+  std::vector<std::pair<int, int>> toSort; // value,size
+  std::string line;
 
   if (file.IsOpen())
   {
     // Read the file
-    std::string line;
     bool isFreeSpace = false;
     int counter = 0;
     while (file.GetLine(line))
@@ -23,6 +25,8 @@ int main(int argc, char const* argv[])
       for (int i = 0; i < line.size(); ++i)
       {
         int value = line[i] - '0';
+        if (i % 2 == 0)
+          toSort.push_back(std::make_pair(counter, value));
         for (int j = 0; j < value; ++j)
           separatedLine.push_back(isFreeSpace ? -1 : counter);
 
@@ -32,6 +36,60 @@ int main(int argc, char const* argv[])
       }
     }
   }
+
+  while (toSort.size() > 0)
+  {
+    const auto& sortPair = toSort[toSort.size() - 1];
+
+    int freeSpace = 0;
+    for (int i = 0; i < separatedLine.size(); ++i)
+    {
+      if (separatedLine[i] == sortPair.first)
+      {
+        break;
+      }
+      else if (separatedLine[i] == -1)
+      {
+        freeSpace++;
+        if (freeSpace >= sortPair.second)
+        {
+          // std::cout << "Found at " << i << ' ' << separatedLine[i] << ' ' << freeSpace << ' ' << sortPair.first << ' ' << sortPair.second << std::endl;
+          for (int& rVal : separatedLine)
+            if (rVal == sortPair.first)
+              rVal = -1;
+          for (int j = i - freeSpace + 1; j <= i; ++j)
+            separatedLine[j] = sortPair.first;
+          break;
+        }
+      }
+      else
+      {
+        freeSpace = 0;
+      }
+    }
+
+    toSort.pop_back();
+    // break;
+  }
+
+  // for (auto i : separatedLine)
+  // {
+  //   if (i == -1)
+  //     std::cout << '.';
+  //   else
+  //     std::cout << i;
+  // }
+  // std::cout << std::endl;
+
+  uint64_t result = 0;
+  for (int i = 0; i < separatedLine.size(); ++i)
+  {
+    if (separatedLine[i] != -1)
+    {
+      result += separatedLine[i] * i;
+    }
+  }
+  std::cout << "Result: " << result << std::endl;
 
   return 0;
 }
