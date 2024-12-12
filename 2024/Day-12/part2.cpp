9343a99237d1012dd377e99c241913f648da4c94
David Bergevoet <david@rometron.nl>
2024-12-12 20:46:58 +0100

    Im a certified gardener now

--- 2024/Day-12/part2.cpp
+++ 2024/Day-12/part2.cpp
@@ -17,24 +17,20 @@ std::ostream& operator<<(std::ostream& rOs, const Plot_t& rPlot)
 
 Matrix<Plot_t> garden;
 
-void GetAreaPerimeterOfPoint(const Point_t& rPoint, std::pair<uint, uint>& rAreaPerimeterPair)
+void GetAreaPerimeterOfPoint(const Point_t& rPoint, std::vector<Point_t>& rPlot)
 {
   if (!garden[rPoint].visited)
   {
-    rAreaPerimeterPair.first++;
+    rPlot.push_back(rPoint);
+
     garden[rPoint].visited = true;
     const auto adjacentPoints = garden.GetAdjacentPoints(rPoint, false);
-    const auto diagonalPoints = garden.GetAdjacentDiagonalPoints(rPoint);
 
     for (const Point_t& rAdjPoint : adjacentPoints)
     {
-      if (garden[rAdjPoint].character != garden[rPoint].character)
+      if (garden[rAdjPoint].character == garden[rPoint].character && !garden[rAdjPoint].visited)
       {
-        rAreaPerimeterPair.second++;
-      }
-      else if (!garden[rAdjPoint].visited)
-      {
-        GetAreaPerimeterOfPoint(rAdjPoint, rAreaPerimeterPair);
+        GetAreaPerimeterOfPoint(rAdjPoint, rPlot);
       }
     }
   }
@@ -65,18 +61,79 @@ int main(int argc, char const* argv[])
 
   uint64_t total = 0;
 
+  std::vector<std::vector<Point_t>> plots;
+
   for (const Point_t& rPoint : garden.GetAllPoints())
   {
     if (!garden[rPoint].visited)
     {
-      std::pair<uint, uint> areaPerimeterPair;
-      GetAreaPerimeterOfPoint(rPoint, areaPerimeterPair);
-      total += areaPerimeterPair.first * areaPerimeterPair.second;
-      // std::cout << garden[rPoint].character << " " << areaPerimeterPair.first << ' ' << areaPerimeterPair.second << std::endl;
+      std::vector<Point_t> plot;
+      GetAreaPerimeterOfPoint(rPoint, plot);
+      plots.push_back(plot);
     }
   }
 
-  // std::cout << garden << std::endl;
+  for (const std::vector<Point_t>& rPlot : plots)
+  {
+    uint plotCorners = 0;
+    for (const Point_t& rPoint : rPlot)
+    {
+      const auto adjPoints = garden.GetAdjacentPoints(rPoint, false);
+      const auto diagPoints = garden.GetAdjacentDiagonalPoints(rPoint);
+      uint counter = 0;
+      bool hasTop = false, hasBottom = false, hasRight = false, hasLeft = false;
+      bool hasTopLeft = false, hasTopRight = false, hasBottomRight = false, hasBottomLeft = false;
+
+      for (const Point_t& rDiagPoint : diagPoints)
+      {
+        if (garden[rDiagPoint].character == garden[rPoint].character)
+        {
+          if (rDiagPoint.first == rPoint.first - 1 && rDiagPoint.second == rPoint.second - 1)
+            hasTopLeft = true;
+          if (rDiagPoint.first == rPoint.first + 1 && rDiagPoint.second == rPoint.second + 1)
+            hasBottomRight = true;
+          if (rDiagPoint.first == rPoint.first - 1 && rDiagPoint.second == rPoint.second + 1)
+            hasTopRight = true;
+          if (rDiagPoint.first == rPoint.first + 1 && rDiagPoint.second == rPoint.second - 1)
+            hasBottomLeft = true;
+        }
+      }
+      for (const Point_t& rAdjPoint : adjPoints)
+      {
+        if (garden[rAdjPoint].character == garden[rPoint].character)
+        {
+          if (rAdjPoint.first == rPoint.first - 1)
+            hasTop = true;
+          if (rAdjPoint.first == rPoint.first + 1)
+            hasBottom = true;
+          if (rAdjPoint.second == rPoint.second + 1)
+            hasRight = true;
+          if (rAdjPoint.second == rPoint.second - 1)
+            hasLeft = true;
+        }
+      }
+      if (!hasTop && !hasRight)
+        plotCorners++;
+      if (!hasRight && !hasBottom)
+        plotCorners++;
+      if (!hasBottom && !hasLeft)
+        plotCorners++;
+      if (!hasLeft && !hasTop)
+        plotCorners++;
+
+      if (hasTop && hasLeft && !hasTopLeft)
+        plotCorners++;
+      if (hasTop && hasRight && !hasTopRight)
+        plotCorners++;
+      if (hasBottom && hasLeft && !hasBottomLeft)
+        plotCorners++;
+      if (hasBottom && hasRight && !hasBottomRight)
+        plotCorners++;
+    }
+
+    // std::cout << "PlotCorners: " << garden[rPlot[0]].character << " " << plotCorners << std::endl;
+    total += plotCorners * rPlot.size();
+  }
 
   std::cout << "Total: " << total << std::endl;
 
