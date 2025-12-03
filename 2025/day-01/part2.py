c6ab952a23a8ddad20a434930e91b01668fc4eef
David Bergevoet <david@rometron.nl>
2025-12-03 05:51:25 +0100

    Part2 of 2025!

---
+++ 2025/day-01/part2.py
@@ -0,0 +1,29 @@
+import os
+
+def get_lines():
+	with open("input.txt") as file:
+	    return [line.rstrip() for line in file]
+
+
+start_value = 50
+total_zeros = 0
+
+for line in get_lines():
+	line_value = int(line[1:])
+
+	for i in range(0, line_value):
+		if line[0] == 'R':
+			start_value += 1
+		if line[0] == 'L':
+			start_value -= 1
+
+		if start_value < 0:
+			start_value = 99
+		elif start_value == 100:
+			start_value = 0
+		# print(f"{line_value} \t: {start_value}")
+
+		if start_value == 0:
+			total_zeros += 1
+
+print(f"Total zeros: {total_zeros}")
